13bcbb419d2738346a2c61d676fc2876c8721b36
Yaseen <yasjbr@yahoo.com>
2019-06-18 12:48:04 +0300

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { New2Component } from './new2/new2.component';
+import { Service1Service } from './service1.service';
+import { Service2Service } from './service2.service';
+import {enableProdMode} from '@angular/core';
+
+enableProdMode();
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'train2';
+  name = 'yaseen';
+  
+
+  constructor(private Service1: Service1Service, Service2: Service2Service) {
+    Service2.typeSomeThing('name');
+    this.printOnConsole();
+  }
+
+  printOnConsole(){
+    console.log('+++'+this.name);
+  }
+
+  show(){
+    console.log(this.name);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewComponent } from './new/new.component';
+import { New2Component } from './new2/new2.component';
+import { Service1Service } from './service1.service';
+import { New3Component } from './new3/new3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,NewComponent, New2Component, New3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [Service1Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
